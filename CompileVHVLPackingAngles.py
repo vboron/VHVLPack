decf7c61955d383f86cc3706d08a061df2fb7b75
vboron <ucbtvbo@ucl.ac.uk>
2021-04-07 16:38:36 +0100

    Cleaned up version: produces .csv file for all pdb angles in a directory

--- CompileVHVLPackingAngles.py
+++ CompileVHVLPackingAngles.py
@@ -5,22 +5,24 @@ File:       VH_VL_Angle_Compiler.py
 
 Version:    V1.1
 Date:       15.03.2021
-Function:   Calculate angles between VH and VL domains for all PDB files availble
+Function:   Calculate packing angles between VH and VL domains for all PDB files available and output csv.
 
 Description:
 ============
 The program takes a directory of Chothia numbered PDB files of antibodies and uses the coordinates to calculate
-the packing angle between the VH and VL domains and outputs a list of names with the angles.
+the packing angle between the VH and VL domains and outputs a .csv file containing columns with the pdb code and the
+packing angle
 e.g.
-5I5K_2: -43.150640
-3I02_1: -44.048283
+      pdb       angle
+   6NOV_2  -44.016817
 
 --------------------------------------------------------------------------
 """
 # *************************************************************************
 # Import libraries
 
-# sys to take args from commandline, os for reading directory, and subprocess for running external program
+# sys to take args from commandline, os for reading directory, subprocess for running external program, pandas
+# for making dataframes
 import os
 import subprocess
 import sys
@@ -31,7 +33,7 @@ import pandas as pd
 def get_pdbdirectory():
     """Read the directory name from the commandline argument
 
-    Return: pdb_directory      --- Directory of PBD files that will be processed for VH-VL packing angles
+    Return: pdb_direct      --- Directory of PBD files that will be processed for VH-VL packing angles
 
     15.03.2021  Original   By: VAB
     """
@@ -46,7 +48,7 @@ def get_pdbdirectory():
 
 # *************************************************************************
 def read_directory_for_pdb_files(pdb_direct):
-    """Print a list of all files that are PDB files in the called directory
+    """Return a list of all files that are PDB files in the called directory
 
     Input:  pdb_direct   --- Directory of PBD files that will be processed for VH-VL packing angles
     Return: files        --- All PDB files in the directory
@@ -96,30 +98,29 @@ def run_abpackingangle(pdb_files, generate_pdb_names):
     5WKO_4: -43.998193
     3U0T_1: -35.507964
 
-        Input:  pdb_files    --- All PDB files in the directory
-                pdb_name     --- Names of all PDB files in the directory
-
+        Input:  pdb_files         --- All PDB files in the directory
+                pdb_name          --- Names of all PDB files in the directory
+        Return: angle_results     --- List of angles corresponding to pdb codes
+            e.g. ['3U0T_1: -35.507964', '5V6M_1: -41.396929', ...]
 
         19.03.2021  Original   By: VAB
         """
+    angle_results = []
 
-    # Opens an txt file in binary writing mode and feeds the file output into it
-    with open('out.txt', 'w') as outfile:
-        # Takes the two lists made in files and combines them into lists of tuples that have
-        # the name linked to the file.
-        for pdb_file, pdb_code in zip(pdb_files, generate_pdb_names):
+    # Takes the two lists made in files and combines them into lists of tuples that have
+    # the name linked to the file.
+    for pdb_file, pdb_code in zip(pdb_files, generate_pdb_names):
 
-            # Uses the subprocess module to call abpackingangle and inputs the headers/.pdb lists
-            # into the program as arguments
-            try:
-                angle_results = subprocess.check_output(['abpackingangle', '-p', pdb_code, '-q', pdb_file])
+        # Uses the subprocess module to call abpackingangle and inputs the headers/.pdb lists
+        # into the program as arguments
+        try:
+            angle_result = subprocess.check_output(['abpackingangle', '-p', pdb_code, '-q', pdb_file])
 
-            # bypasses any files that raise an error and the abpackingangle cannot run
-            except subprocess.CalledProcessError:
-                continue
-            # Converts the output of the subprocess into normal string
-            angle_results = str(angle_results, 'utf-8')
-            outfile.write(angle_results)
+        # bypasses any files that raise an error and the abpackingangle cannot run
+        except subprocess.CalledProcessError:
+            continue
+        # Converts the output of the subprocess into normal string
+        angle_results.append(str(angle_result, 'utf-8'))
     return angle_results
 
 
@@ -128,7 +129,9 @@ def convert_to_csv(angles):
     """Break the lines resulting from the abpackingangle function and convert them into a csv table
 
         Input:  angles    --- lines containing the pdb name and the packing angle separated by ':'
-
+        Return: atable    --- dataframe containing all of the pdb codes and their packing angles
+            e.g.      pdb       angle
+                   6NOV_2  -44.016817
 
         07.04.2021  Original   By: VAB
         """
@@ -136,8 +139,8 @@ def convert_to_csv(angles):
     c = ['pdb', 'angle']
     table = []
 
-    # lines are split into objects in the line by the ':' and assigned variable names
     for line in angles:
+        # lines are split into objects in the line by the ':' and assigned variable names
         pdb_name = (line.split(':')[0]).strip()
         angle = (line.split(':')[1]).strip()
         angle_info = [pdb_name, angle]
@@ -154,13 +157,10 @@ def convert_to_csv(angles):
 # *************************************************************************
 
 pdb_directory = get_pdbdirectory()
-# print(pdb_direct)
 
 all_pdb_files = read_directory_for_pdb_files(pdb_directory)
-# print('files', pdb_files)
 
 all_pdb_names = extract_pdb_name(pdb_directory)
-# print('generate_pdb_names', generate_pdb_names)
 
 calculate_angles = run_abpackingangle(all_pdb_files, all_pdb_names)
 
