b49d67c2e4d9d0983bfad88a649825a7f5250fde
vboron <78688471+vboron@users.noreply.github.com>
2021-04-07 16:56:51 +0100

    Add files via upload

---
+++ graphVHVLangles.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Program:    graphVHVLangles
+File:       graphVHVLangles.py
+
+Version:    V1.0
+Date:       07.04.2021
+Function:
+
+Description:
+============
+
+
+--------------------------------------------------------------------------
+"""
+# *************************************************************************
+# Import libraries
+
+# sys to take args from commandline
+import sys
+
+
+# *************************************************************************
+def read_file():
+
+# Take the commandline input as the file to search for packing angle data
+    if sys.argv[1] != '':
+        angle_file = sys.argv[1]
+
+    return angle_file
+
+
+# *************************************************************************
+def cluster_angles_by_value():
+    for
+
+# *************************************************************************
+# *** Main program                                                      ***
+# *************************************************************************
+
+datatable = read_file()
+
+clustered_data = cluster_angles_by_value(datatable)
