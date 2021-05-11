540c927e2f208858eaeceeae900c7540b8f1ca69
vboron <ucbtvbo@ucl.ac.uk>
2021-05-11 13:12:14 +0100

    generalized for all encoding types

--- remove_redundancy.py
+++ remove_redundancy.py
@@ -19,9 +19,6 @@ a table with the first column being a list of all of the PDB codes, followed by 
 
 import pandas as pd
 import sys
-sys.path.append('/serv/www/html_lilian/libs')
-sys.path.append('./CDRH3lib')
-sys.path.append('~/sync_project/WWW/CDRH3loop')
 
 
 # *************************************************************************
@@ -35,21 +32,21 @@ def remove_duplicates():
     01.05.2021  Original   By: VAB
     """
 
-    # The column names contained in the .csv file
-    col1 = ['code', 'L38a', 'L38b', 'L38c', 'L38d', 'L38e', 'L40a', 'L40b', 'L40c', 'L40d', 'L40e', 'L41a', 'L41b',
-            'L41c', 'L41d', 'L41e', 'L44a', 'L44b', 'L44c', 'L44d', 'L44e',
-            'L46a', 'L46b', 'L46c', 'L46d', 'L46e', 'L87a', 'L87b', 'L87c', 'L87d', 'L87e',
-            'H33a', 'H33b', 'H33c', 'H33d', 'H33e', 'H42a', 'H42b', 'H42c', 'H42d', 'H42e',
-            'H45a', 'H45b', 'H45c', 'H45d', 'H45e', 'H60a', 'H60b', 'H60c', 'H60d', 'H60e',
-            'H62a', 'H62b', 'H62c', 'H62d', 'H62e', 'H91a', 'H91b', 'H91c', 'H91d', 'H91e',
-            'H105a', 'H105b', 'H105c', 'H105d', 'H105e', 'angle']
+    # The column names contained in the .csv file imported from a .dat file
+    col1 = []
+    for i in open(sys.argv[2]).readlines():
+        i = i.strip('\n')
+        col1.append(i)
 
     # Take the commandline input as the directory, otherwise look in current directory
     if sys.argv[1] != '':
         res_file = pd.read_csv(sys.argv[1], usecols=col1)
 
     # remove lines that don't contain angles
-    res_file = res_file[res_file['angle'].str.contains('Packing angle') == False]
+    try:
+        res_file = res_file[res_file['angle'].str.contains('Packing angle') == False]
+    except:
+        print('No missing angles.')
 
     # angle needs to be converted to a float
     res_file['angle'] = res_file['angle'].astype(float)
@@ -58,24 +55,11 @@ def remove_duplicates():
     # make a list of all combined pdb names separated by commas and add them as a value
     agg_func = {'code': ', '.join}
 
-    seq_df = res_file.groupby(['L38a', 'L38b', 'L38c', 'L38d', 'L38e', 'L40a', 'L40b', 'L40c', 'L40d', 'L40e',
-                               'L41a', 'L41b', 'L41c', 'L41d', 'L41e', 'L44a', 'L44b', 'L44c', 'L44d', 'L44e',
-                               'L46a', 'L46b', 'L46c', 'L46d', 'L46e', 'L87a', 'L87b', 'L87c', 'L87d', 'L87e',
-                               'H33a', 'H33b', 'H33c', 'H33d', 'H33e', 'H42a', 'H42b', 'H42c', 'H42d', 'H42e',
-                               'H45a', 'H45b', 'H45c', 'H45d', 'H45e', 'H60a', 'H60b', 'H60c', 'H60d', 'H60e',
-                               'H62a', 'H62b', 'H62c', 'H62d', 'H62e', 'H91a', 'H91b', 'H91c', 'H91d', 'H91e',
-                               'H105a', 'H105b', 'H105c', 'H105d', 'H105e', 'angle']).aggregate(agg_func)
+    seq_df = res_file.groupby(col1[1:]).aggregate(agg_func)
     seq_df = seq_df.reset_index()
 
     # columns were re-ordered so that the PDB code would be first
-    reorder_col = ['code', 'L38a', 'L38b', 'L38c', 'L38d', 'L38e', 'L40a', 'L40b', 'L40c', 'L40d', 'L40e',
-                   'L41a', 'L41b', 'L41c', 'L41d', 'L41e', 'L44a', 'L44b', 'L44c', 'L44d', 'L44e',
-                   'L46a', 'L46b', 'L46c', 'L46d', 'L46e', 'L87a', 'L87b', 'L87c', 'L87d', 'L87e',
-                   'H33a', 'H33b', 'H33c', 'H33d', 'H33e', 'H42a', 'H42b', 'H42c', 'H42d', 'H42e',
-                   'H45a', 'H45b', 'H45c', 'H45d', 'H45e', 'H60a', 'H60b', 'H60c', 'H60d', 'H60e',
-                   'H62a', 'H62b', 'H62c', 'H62d', 'H62e', 'H91a', 'H91b', 'H91c', 'H91d', 'H91e',
-                   'H105a', 'H105b', 'H105c', 'H105d', 'H105e', 'angle']
-    seq_df = seq_df[reorder_col]
+    seq_df = seq_df[col1]
     seq_df = seq_df.sort_values('code')
 
     return seq_df
