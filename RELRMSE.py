9f314c186ea203f80569c9816ee3ae7df7f07a72
vboron <ucbtvbo@ucl.ac.uk>
2021-05-12 13:20:16 +0100

    Added commandline input descriptors

--- RELRMSE.py
+++ RELRMSE.py
@@ -12,6 +12,8 @@ Description:
 Program takes a .csv file containing the pdb code as 'code' and the non-normalized packing angle as 'angle', and the
 RMSE calculated by program as the second input.
 ------------------------------------------------
+Commandline inputs: 1) file containing the angles for the PDB files
+                    2) the RMSE
 """
 
 
@@ -23,7 +25,10 @@ import numpy as np
 
 
 # *************************************************************************
-def calc_relemse():
+def open_csv():
+
+# *************************************************************************
+def calc_relemse(df):
     """Read the .csv for angles and take the RMSE from the commandline to calculate the relative RMSE.
     Equation used: RELRMSE = (RMSE*(n**(1/2)))/((sum (angle**2))**(1/2))
 
@@ -32,35 +37,28 @@ def calc_relemse():
     05.05.2021  Original   By: VAB
     """
 
-    # The column names contained in the .csv file
-    col1 = ['code', 'angle']
-
-    # Read the .csv files from the commandline and convert it into a dataframe
-    if sys.argv[1] != '':
-        data = pd.read_csv(sys.argv[1], usecols=col1)
-
     # Take the rmse from the second commandline input and convert it into a float
     rmse = float(sys.argv[2])
 
     # Count the number of angles in the dataframe
-    n = data['angle'].count()
+    n = df['angle'].count()
 
     # Take the square root of the number of angles
     rn = float(n)**(1/2)
 
     # Makes a list of all the angles squared
     sq_angle = []
-    for angle in data['angle']:
+    for angle in df['angle']:
         angle_2 = float(angle)**2
         sq_angle.append(angle_2)
 
     col2 = ['sqangle']
 
     # Create a dataframe of all angles squared
-    df = pd.DataFrame(data=sq_angle, columns=col2)
+    df2 = pd.DataFrame(data=sq_angle, columns=col2)
 
     # Take the sum of all the square angles
-    c_sangles = df['sqangle'].sum()
+    c_sangles = df2['sqangle'].sum()
 
     # Take the sqroot of summed square angles
     rc_sangles = float(c_sangles)**(1/2)
@@ -70,5 +68,14 @@ def calc_relemse():
 
 
 # *************************************************************************
-result = calc_relemse()
+# Main Program
+# *************************************************************************
+# The column names contained in the .csv file
+col1 = ['code', 'angle']
+
+# Read the .csv files from the commandline and convert it into a dataframe
+if sys.argv[1] != '':
+    data = pd.read_csv(sys.argv[1], usecols=col1)
+
+result = calc_relemse(data)
 print(result)
