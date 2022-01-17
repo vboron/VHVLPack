b15c6dbbd43c70715ef5f540906473d13d481d1a
vboron <ucbtvbo@ucl.ac.uk>
2022-01-17 11:01:36 +0000

    add snns functions to program

--- packing_angle_pipeline.py
+++ packing_angle_pipeline.py
@@ -1,4 +1,5 @@
 from enum import Enum, auto
+import os
 import argparse
 import utils
 
@@ -29,7 +30,6 @@ def preprocessing(ds: Dataset):
     run_compile_angles(ds)
     run_find_VHVLres(ds)
     run_encode_4d(ds)
-    # TODO more things
 
 def run_compile_angles(ds: Dataset):
     utils.run_cmd(['./compile_angles.py', '--directory', ds.name, '--csv-fil', f'{ds.name}_ang'], args.dry_run)
@@ -38,7 +38,7 @@ def run_find_VHVLres(ds: Dataset):
     utils.run_cmd(['./find_VHVLres.py', ds.name, f'{ds.name}_res'], args.dry_run)
 
 def run_encode_4d(ds: Dataset):
-    utils.run_cmd([f'{ds.name}_res', f'{ds.name}_ang', '4d.dat', f'{ds.name}_4d', f'{ds.name}'])
+    utils.run_cmd([f'{ds.name}_res.csv', f'{ds.name}_ang.csv', '4d.dat', f'{ds.name}_4d', f'{ds.name}'])
 
 def process(ds: Dataset, nr: NonRedundantization, meth: MLMethod, cf: CorrectionFactor):
     unique_name = f"{ds.name}_{nr.name}_{meth.name}_{cf.name}"
@@ -47,6 +47,26 @@ def process(ds: Dataset, nr: NonRedundantization, meth: MLMethod, cf: Correction
     if cf == CorrectionFactor.Yes:
         # zrob ponownie costam
         pass
+
+def run_papa(ds: Dataset, nr: NonRedundantization, meth: MLMethod):
+    utils.run_cmd(['./snns_run_and_compile_data.py', os.path.join(ds.name, 'seq_files'), '4d.dat', 
+    f'{ds.name}_ang.csv', f'{ds.name}', 'papa'])
+
+def run_newpapa(ds: Dataset, nr: NonRedundantization, meth: MLMethod):
+    utils.run_cmd(['csv2arff', '-norm', '-ni', 'in4d.dat', 'angle', f'{ds.name}_{nr.name}_4d.csv', 
+    f'> {ds.name}_{nr.name}_{meth.name}.arff'])
+    pat_path = os.path.join('SNNS', 'papa', 'training', 'final.pat')
+    utils.run_cmd(['arff2snns', f'{ds.name}_{nr.name}_{meth.name}.arff', f'> {pat_path}'])
+    utils.run_cmd(['batchman', '-f', f'> {pat_path}'])
+    install_path = os.path.join('SNNS', 'papa', 'training', 'install.sh')
+    utils.run_cmd([f'./{install_path}', f'$HOME/{ds.name}_{nr.name}_{meth.name}'])
+    utils.run_cmd(['./snns_run_and_compile_data.py', os.path.join(ds.name, 'seq_files'), '4d.dat', 
+    f'{ds.name}_ang.csv', f'{ds.name}', f'~/{ds.name}_{nr.name}_{meth.name}/papa'])
+
+def run_snns(ds: Dataset):
+    utils.run_cmd(['./pdb2seq.py', ds.name])
+    # distinguish between making a new papa and running the old papa
+
 
 parser = argparse.ArgumentParser(description='Program for compiling angles')
 parser.add_argument('--dry-run', action='store_true')
