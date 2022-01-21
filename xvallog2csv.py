65cda5c469cf0aaa15ad23b3393fb411f4e5331c
vboron <ucbtvbo@ucl.ac.uk>
2022-01-21 15:18:45 +0000

    finished script for extracting statistical data from log files of cross validated MLP

--- xvallog2csv.py
+++ xvallog2csv.py
@@ -1,12 +1,18 @@
-import argsparser
+#!/usr/bin/env python3
+"""
+Function:   Produce a table of statisics from crossvalidated MLP test data.
+Description:
+============
+Program searches for the test log files and extracts the lines containing the statistics like error and RMSE. These
+stats are then put into a pandas DataFrame and exported as a .csv file.
+------------------------------------------------
+"""
+import argparse
 import pandas as pd
 import os
+import re
 
-def extract_xval_stats(columns, directory):
-    col = []
-    for i in open(columns).readlines():
-        i = i.strip('\n')
-        col.append(i)
+def extract_xval_stats(directory, columns, output_name):
 
     all_data = []
     files = []
@@ -16,14 +22,38 @@ def extract_xval_stats(columns, directory):
         if file.endswith("_test.log"):
             files.append(os.path.join(directory, file))
 
+    stats_to_get = ['Correlation coefficient', 'Mean absolute error', 'Root mean squared error',
+                    'Total Number of Instances']
+
     # Open each log file in the directory and find the relevant information
+    all_data = []
     for log_file in files:
-        with open(log_file) as text_file:
-            for line in text_file:
+        with open(log_file) as f:
+            stat_lines = []
+            lines = f.readlines()
+            name = log_file.split('_')[1]
+            stat_lines.append(name)
+            for line in lines:
+                if str(line).strip().startswith(tuple(stats_to_get)):
+                    line = re.split('[a-z\\s]+', line, flags=re.IGNORECASE)
+                    line = line[1]
+                    stat_lines.append(line)
+            all_data.append(stat_lines)
 
+    stat_cols = []
+    for i in open(columns).readlines():
+        i = i.strip('\n')
+        stat_cols.append(i)
 
-    # Make .csv files for all of the data, splitting it into files that have all the data, only outliers, and only the
-    # data withing the 'norm'
-    df_a = pd.DataFrame(data=all_data, columns=col)
-    path = os.path.join(direct, f'{csv_out}.csv')
+    df_a = pd.DataFrame(data=all_data, columns=stat_cols)
+    path = os.path.join(directory, f'{output_name}.csv')
     df_a.to_csv(path, index=False)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Program for extracting VH/VL relevant residues')
+    parser.add_argument('--directory', help='Directory where log files are', required=True)
+    parser.add_argument('--xval_cols', help='.dat columns for processing xval MLP outputs', required=True)
+    parser.add_argument('--out_csv', help='Name for the .csv output', required=True)
+    args = parser.parse_args()
+
+    extract_xval_stats(args.directory, args.xval_cols, args.out_csv)
