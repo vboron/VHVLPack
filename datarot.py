981c5d501a81356348117328d3845b5db20c1f1c
vboron <ucbtvbo@ucl.ac.uk>
2022-05-16 21:30:11 +0100

    Temp

--- datarot.py
+++ datarot.py
@@ -109,7 +109,7 @@ def RotateDataArray(xData, yData, nData, theta):
 
 def CorrectAndPrintDataFile(dataFile, m, c):
     df = pd.read_csv(dataFile)
-    ndata = df['code'].count()
+    ndata = df['Y'].count()
     xData = df['angle'].tolist()
     xDataOrig = df['angle'].tolist()
     yData = df['predicted'].tolist()
