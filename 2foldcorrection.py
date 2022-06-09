7dc39d5a75791c263ec4993c91ade5d810b3d7c5
vboron <ucbtvbo@ucl.ac.uk>
2022-06-09 14:20:28 +0100

    Save editing of correction script

--- 2foldcorrection.py
+++ 2foldcorrection.py
@@ -9,8 +9,9 @@ import graphing
 import stats2graph
 
 
-def find_norms_and_outliers(csv_file):
-    df = pd.read_csv(csv_file)
+def find_norms_and_outliers(directory, csv_file):
+    path_csv_file = os.path.join(directory, csv_file)
+    df = pd.read_csv(path_csv_file)
 
     min_norm = -48
     max_norm = -42
@@ -24,7 +25,39 @@ def find_norms_and_outliers(csv_file):
 
     df_outliers = pd.concat([outliers_max, outliers_min])
 
+    df_outliers.to_csv(os.path.join(directory, 'Everything_NR2_GBReg_out.csv'))
+
     return df_normal, df_outliers
 
-def run_norm_correction(df_normal):
-    
+def run_norm_correction(directory, df_normal, first_m, first_c):
+    df_normal.to_csv(os.path.join(directory, 'Everything_NR2_GBReg_norm.csv'))
+    m = first_m
+    c = first_c
+    for i in range(1, 6):
+        # TODO change name here
+        file_name = f'NR2_GBReg_correction_{i}'
+        
+        csv_name = f'{file_name}.csv'
+        path_name = os.path.join(directory, csv_name)
+
+        # TODO fix this line here to work for this case
+        cmds = ['./datarot.py', '-o', path_name, '-m', str(m), '-c', str(c), '--dataFile', datafile]
+
+        utils.run_cmd(cmds, False)
+        df = pd.read_csv(path_name)
+        df['abs_err'] = df['error'].abs()
+        df['sqerror'] = df['error'].pow(2)
+        df['sq_angle'] = df['angle'].pow(2)
+        sum_sqe = df['sqerror'].sum()
+        n = df['angle'].count()
+        meanabserror = (df['abs_err'].sum())/n
+        rmsd = math.sqrt(sum_sqe/n)
+        relrmse = utils.calc_relemse(path_name, rmsd)
+        # print(df, sum_sqe, n, meanabserror, rmsd, relrmse)
+
+        graphing.error_distribution(directory, csv_name, f'error_dist_correction_{i}')
+        m, c = stats2graph.create_stats_and_graph(args.directory, csv_name, file_name, file_name)
+        datafile = path_name
+        stats = [meanabserror, rmsd, relrmse, m, c]
+        stats_df = pd.DataFrame(data=[stats], columns=['meanabserror', 'rmsd', 'relrmse', 'm', 'c'])
+        print(stats_df)
