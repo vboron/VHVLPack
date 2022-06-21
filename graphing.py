6d54bed8237e4a40d0188d8aacbbacfa0496a5ec
vboron <ucbtvbo@ucl.ac.uk>
2022-06-21 15:00:55 +0100

    Add function that plots pred vs actual angle from df

--- graphing.py
+++ graphing.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import math
+import utils
 
 
 # *************************************************************************
@@ -102,3 +103,77 @@ def sq_error_vs_actual_angle(directory, csv_input, graph_name):
        path_fig = os.path.join(directory, f'{graph_name}.jpg')
        plt.savefig(path_fig, format='jpg')
        plt.close()
+
+# *************************************************************************
+
+def actual_vs_predicted_from_df(df, directory, stats_csv_name, pa_graph_name):
+
+       # Calculate the Root Mean Square Error
+       df['sqerror'] = np.square(df['error'])
+       sum_sqerror = df['sqerror'].sum()
+       average_error = sum_sqerror / int(df['angle'].size)
+       rmse = math.sqrt(average_error)
+
+       relrmse = utils.calc_relemse_from_df(df, rmse)
+
+       # .corr() returns the correlation between two columns
+       pearson_a = df['angle'].corr(df['predicted'])
+
+       mean_abs_err = df['error'].abs().mean()
+       stat_data = [pearson_a, mean_abs_err, rmse, relrmse]
+       stat_col = ['pearson', 'error', 'RMSE', 'RELRMSE']
+       stats = pd.DataFrame(data=[stat_data], columns=stat_col)
+
+       plt.figure()
+
+       color_values = 'mediumpurple'
+       color_bf_line = 'rebeccapurple'
+
+       x = df['angle']
+       y = df['predicted']
+
+       m, b = np.polyfit(x, y, 1)
+       plt.plot(x, m * x + b, color=color_bf_line, linestyle='dashed', linewidth=1)
+
+       plt.scatter(x, y, s=2, color=color_values)
+
+       axes = plt.gca()
+
+       # Sets the maximum and minimum values for the axes
+       # axes.autoscale(tight=True)
+       axes.set_xlim([-65, -25])
+       axes.set_ylim([-65, -25])
+
+       axes.axline((0, 0), (1, 1), color='k')
+
+       # Sets the axes labels
+       plt.xlabel('Actual interface angle')
+       plt.ylabel('Predicted interface angle')
+
+       # Adds graph annotations
+       plt.text(s='Line: y=x', x=-61, y=-32, fontsize=8)
+
+       plt.text(s='Best fit: y={:.3f}x+{:.3f}'.format(m, b),
+              x=-61, y=-33, fontsize=8, color=color_bf_line)
+       plt.text(s='RELRMSE: {:.3}'.format(
+              float(stats['RELRMSE'])), x=-61, y=-34, fontsize=8)
+
+       plt.tight_layout()
+
+       # add best fit data to dataframe and export the dataframe
+       # add best fit lines to statistics dataframe
+       bf_line = 'y={:.3f}x+{:.3f}'.format(m, b)
+
+       stats['fit'] = bf_line
+       stats['slope'] = m
+       stats['intercept'] = b
+
+       path_stats = os.path.join(directory, f'{stats_csv_name}.csv')
+       stats.to_csv(path_stats, index=False)
+
+       # Exports the figure as a .jpg file
+       path_fig = os.path.join(directory, f'{pa_graph_name}.jpg')
+       plt.savefig(path_fig, format='jpg')
+       plt.close()
+
+       return m, b
