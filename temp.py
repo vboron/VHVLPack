ed9038a6764beb8f75b2c8b1b1db1fb64bbcfbf9
vboron <ucbtvbo@ucl.ac.uk>
2022-06-30 14:46:21 +0100

    Save

--- temp.py
+++ temp.py
@@ -8,7 +8,8 @@ import math
 import graphing
 
 if __name__ == '__main__':
-    df = pd.read_csv('Everything/Everything_NR2_GBReg.csv')
-    directory = 'Everything'
-    file_name = 'Everything_NR2_GBReg'
-    graphing.actual_vs_predicted_from_df(df, directory, file_name, file_name)
+    path = os.path.join('Everything', 'Everything_NR2_GBReg.csv')
+    df = pd.read_csv(path)
+    pdb_codes = df['code'].to_list()
+    with open(os.path.join('Everything', 'pdb_list.txt'), 'w') as f:
+        f.write(pdb_codes)
