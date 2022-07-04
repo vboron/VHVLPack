7c781806aed3452c035c181a600067a83bcd1439
vboron <ucbtvbo@ucl.ac.uk>
2022-07-04 14:42:13 +0100

    Simplify

--- find_VHVLres.py
+++ find_VHVLres.py
@@ -43,23 +43,32 @@ def read_pdbfiles_as_lines(directory):
             # Prepends the directory path to the front of the file name to create full filepath
             files.append(os.path.join(directory, file))
 
-    pdb_dict = {}
-
+    # pdb_dict = {}
+    atom_lines = []
+    col = ['code', 'L/H position', 'residue']
     for structure_file in files:
-        atom_lines = []
+        
         with open(structure_file, "r") as text_file:
 
             structure_file = structure_file.replace(directory, '')
-            structure_file = structure_file[:-4]
+            pdb_code = structure_file[:-4]
 
             for line in text_file.read().split('\n'):
                 if str(line).strip().startswith('ATOM'):
-                    atom_lines.append(line)
+                    # atom_lines.append(line)
+                    items = line.split()
+                    res_num = items[5]
+                    chain = items[4]
+                    residue = items[3]
+                    lhposition = str(f'{chain}{res_num}')
+                    data = [pdb_code, lhposition, residue]
+                    atom_lines.append(data)
 
-            pdb_dict[structure_file] = atom_lines
+            # pdb_dict[structure_file] = atom_lines
             text_file.close()
-
-    return pdb_dict
+    df = pd.DataFrame(data = data, columns=col)
+    print(df)
+    return df
 
 
 # *************************************************************************
