2fb711e262f75286e84676e292588aed99fbcaf3
vboron <ucbtvbo@ucl.ac.uk>
2022-07-05 13:48:33 +0100

    Fix spelling error

--- utils.py
+++ utils.py
@@ -146,3 +146,41 @@ def stats_for_pred_vs_actual_graph(df):
     stat_col = ['pearson', 'error', 'RMSE', 'RELRMSE']
     stats_df = pd.DataFrame(data=[stat_data], columns=stat_col)
     return stats_df, stat_data
+
+# *************************************************************************
+def nr_side_chain_atoms(resi):
+    # 1. total number of side-chain atoms
+    nr_side_chain_atoms_dic = {'A': 1, 'R': 7, "N": 4, "D": 4, "C": 2, "Q": 5, "E": 5, "G": 0, "H": 6, "I": 4,
+                               "L": 4, "K": 15, "M": 4, "F": 7, "P": 4,
+                               "S": 2, "T": 3, "W": 10, "Y": 8, "V": 3, "X": 10.375}  # "X": 10.375
+    nr_side_chain_atoms = nr_side_chain_atoms_dic[resi]
+    return nr_side_chain_atoms
+
+# *************************************************************************
+def compactness(resi):
+    # 2. number of side-chain atoms in shortest path from Calpha to most distal atom
+    compactness_dic = {'A': 1, 'R': 6, "N": 3, "D": 3, "C": 2, "Q": 4, "E": 4, "G": 0, "H": 4, "I": 3,
+                       "L": 3, "K": 6, "M": 4, "F": 5, "P": 2,
+                       "S": 2, "T": 2, "W": 6, "Y": 6, "V": 2, "X": 4.45}  # , "X": 4.45
+    compactness = compactness_dic[resi]
+    return compactness
+
+# *************************************************************************
+def hydrophobicity(resi):
+    # 3. eisenberg consensus hydrophobicity
+    # Consensus values: Eisenberg, et al 'Faraday Symp.Chem.Soc'17(1982)109
+    Hydrophathy_index = {'A': 00.250, 'R': -1.800, "N": -0.640, "D": -0.720, "C": 00.040, "Q": -0.690, "E": -0.620,
+                         "G": 00.160, "H": -0.400, "I": 00.730, "L": 00.530, "K": -1.100, "M": 00.260, "F": 00.610,
+                         "P": -0.070,
+                         "S": -0.260, "T": -0.180, "W": 00.370, "Y": 00.020, "V": 00.540, "X": -0.5}  # -0.5 is average
+
+    hydrophobicity = Hydrophathy_index[resi]
+    return hydrophobicity
+
+# *************************************************************************
+def charge(resi):
+    dic = {"D": -1, "K": 1, "R": 1, 'E': -1, 'H': 0.5}
+    charge = 0
+    if resi in dic:
+        charge += dic[resi]
+    return charge
