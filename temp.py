a766c146300f7cf668d985132fef8bea0a23a017
vboron <ucbtvbo@ucl.ac.uk>
2022-07-11 09:36:18 +0100

    count nans in df

--- temp.py
+++ temp.py
@@ -8,9 +8,7 @@ import math
 import graphing
 
 if __name__ == '__main__':
-    path = os.path.join('Everything', 'Everything_NR2_GBReg.csv')
-    df = pd.read_csv(path)
-    pdb_codes = df['code'].to_list()
-    with open(os.path.join('Everything', 'pdb_list.txt'), 'w') as f:
-        for item in pdb_codes:
-            f.write(item + '\n')
+    df = pd.read_csv('Everything/VHVL_res_expanded_unencoded.csv')
+    data = df.isna().sum()
+    print(data)
+    
