7ca0eda3fa0b64f388974bca36ae5693fbed97de
vboron <ucbtvbo@ucl.ac.uk>
2022-07-12 12:21:16 +0100

    Add a print

--- encode_res_calc_angles.py
+++ encode_res_calc_angles.py
@@ -36,7 +36,8 @@ def calculate_packing_angles(directory):
                 code = file[:-4]
                 results.append(p.apply_async(run_abpackingangle, (code, os.path.join(directory, file), data_list)))
                 # run_abpackingangle(code, os.path.join(directory, file), data_list)
-                print(results)
+                if not all([r.successful() for r in results]):
+                    print(results)
         p.close()
         p.join()
         if not all([r.successful() for r in results]):
