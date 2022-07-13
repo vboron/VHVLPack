b254bcb0b6d99225af31bcc5531d947a9c837528
vboron <ucbtvbo@ucl.ac.uk>
2022-07-13 12:05:28 +0100

    Save

--- temp.py
+++ temp.py
@@ -32,13 +32,13 @@ def generate_GBReg_model_everything(directory):
     sum_sq_angles = df['sq_angle'].sum()
     relrmse = std*(len(df)**(1/2))/(sum_sq_angles**(1/2))
     print(f'relrmse:{relrmse}, std: {std}, mean error: {mean_error}')
-    # df = df.drop(['sq_angle'])
+    df.drop(['sq_angle'])
     print(df)
 
     df.to_csv('testing_123features_gbr.csv', index=False)
-    graphing.actual_vs_predicted_from_df(df, 'everything_123features')
-    graphing.error_distribution('/', 'testing_123features_gbr.csv', '123features')
-    graphing.sq_error_vs_actual_angle('/', 'testing_123features_gbr.csv', '123features')
+    graphing.actual_vs_predicted_from_df(df, './', 'everything_123features', 'everything_123features_pa')
+    graphing.error_distribution('./', 'testing_123features_gbr.csv', '123features')
+    graphing.sq_error_vs_actual_angle('./', 'testing_123features_gbr.csv', '123features')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
