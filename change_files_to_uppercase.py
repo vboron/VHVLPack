3e97dcaecf434ac5d1e7ea173a0be5001a18ec70
vboron <ucbtvbo@ucl.ac.uk>
2022-07-25 13:13:02 +0100

    Save

--- change_files_to_uppercase.py
+++ change_files_to_uppercase.py
@@ -5,7 +5,8 @@ import subprocess
 
 cwd = os.getcwd()
 for file in os.listdir(sys.argv[1]):
-    if file.endswith('.PDB') or file.endswith('.pdb'):
+    # if file.endswith('.PDB') or file.endswith('.pdb'):
+    if file.endswith('.mar'):
         path1 = os.path.join(cwd, sys.argv[1], file)
-        path2 = os.path.join(cwd, sys.argv[1], (file[:-4].upper() + '.pdb'))
+        path2 = os.path.join(cwd, sys.argv[1], (file[2:-4].upper() + '.pdb'))
         subprocess.run(['mv', path1, path2])
