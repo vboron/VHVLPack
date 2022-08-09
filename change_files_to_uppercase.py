2b29eac18e7ef411dd598cec9f0401ab68086127
vboron <ucbtvbo@ucl.ac.uk>
2022-08-09 10:12:18 +0100

    other correction

--- change_files_to_uppercase.py
+++ change_files_to_uppercase.py
@@ -4,7 +4,10 @@ import sys
 
 cwd = os.getcwd()
 for file in os.listdir(sys.argv[1]):
-    if file.endswith('.mar') or file.endswith('.cho'):
+    # if file.endswith('.mar') or file.endswith('.cho'):
+    #     src = os.path.join(cwd, sys.argv[1], file)
+    #     dst = os.path.join(cwd, sys.argv[1], (file[2:-4].upper() + '.pdb'))
+    if file.endswith('.pdb'):
         src = os.path.join(cwd, sys.argv[1], file)
-        dst = os.path.join(cwd, sys.argv[1], (file[2:-4].upper() + '.pdb'))
+        dst = os.path.join(cwd, sys.argv[1], (file[1:7] + '.pdb'))
         os.rename(src, dst)
