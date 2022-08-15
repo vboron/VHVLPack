a39a983f69c76751ceef125d90a9cc98e808a31b
vboron <ucbtvbo@ucl.ac.uk>
2022-08-15 14:20:29 +0100

    Fix some issues with functions

--- gbr_angle_prediction.py
+++ gbr_angle_prediction.py
@@ -8,58 +8,37 @@ import encode_res_calc_angles as erca
 import nonred
 import graphing
 import gbr_latex as ltp
-from multiprocessing import Pool
 from sklearn_methods import *
 
+
 # *************************************************************************
-
-
 def preprocessing(ds):
-    erca.extract_and_export_packing_residues(ds, ds, 'expanded_residues.dat')
+    encoded_df = erca.extract_and_export_packing_residues(
+        ds, ds, f'{ds}_expanded_residues.dat')
+    nonred_df = nonred.NR2(encoded_df, ds, f'{ds}_NR2_expanded_residues')
+    return nonred_df
 
 
 # *************************************************************************
-def run_nr(dataset):
-  
-    new_file = f'{dataset}_NR2_4d'
-    nonred.NR2(encoded_csv_path, args.cols_4d, dataset, new_file)
-
-# *************************************************************************
-
-
-def runGBReg(trainset, testset):
-    X_train, y_train, _x_ = make_sets(
-        f'{trainset}/{trainset}_NR2_4d.csv')
-    X_test, y_true, df_test = make_sets(
-        f'{testset}/{testset}_NR2_4d.csv')
-    print(f'Running GBRegressor on {testset}')
+def runGBReg(train_df, test_df, model_name):
+    X_train, y_train, _x_ = make_sets_from_df(train_df)
+    X_test, y_true, df_test = make_sets_from_df(test_df)
     df = run_GradientBoostingRegressor(
-        X_train, y_train, X_test, df_test, 'af2clean_trained_gbr_123features')
-    df.to_csv(f'{testset}/{testset}_NR2_GBReg.csv', index=False)
+        X_train, y_train, X_test, df_test, model_name)
+    df.to_csv(f'results_for_{model_name}', index=False)
+    return df
+
 
 # *************************************************************************
-
-
-def run_graphs(dataset, name):
-    cmd = ['./stats2graph.py',
-           '--directory', dataset, '--csv_input', f'{name}.csv', '--name_normal',
-           f'{name}_normal', '--name_outliers', f'{name}_normal',
-           '--name_stats', f'{name}_stats', '--name_graph', name]
-    utils.run_cmd(cmd, False)
+def postprocessing(dataset, df):
+    name = 'train_everything_test_abdbnew'
+    graphing.actual_vs_predicted_from_df(df, dataset, name, f'{name}_pa')
     graphing.sq_error_vs_actual_angle(
         dataset, f'{name}.csv', f'{name}_sqerror_vs_actual')
     graphing.angle_distribution(
         dataset, f'{dataset}_ang.csv', f'{name}_angledistribution')
     graphing.error_distribution(
         dataset, f'{name}.csv', f'{name}_errordistribution')
-    graphing.sq_error_vs_actual_angle(
-        dataset, f'{name}.csv', f'{name}_sqerror_vs_actual')
-
-
-def postprocessing(dataset):
-    print(f'Postprocessing {dataset}')
-    name = f'{dataset}_NR2_GBReg'
-    return run_graphs(dataset, name)
 
 
 # *************************************************************************
@@ -68,6 +47,8 @@ parser.add_argument('--preprocess', action='store_true', default=False)
 parser.add_argument('--process', action='store_true', default=False)
 parser.add_argument('--postprocess', action='store_true', default=False)
 parser.add_argument('--latex', action='store_true', default=False)
+parser.add_argument(
+    '--name_model', action='name that the trained model will be stored under', default=False)
 
 args = parser.parse_args()
 
@@ -77,26 +58,19 @@ if not args.preprocess and not args.process and not args.postprocess and not arg
 
 
 if args.preprocess:
-    def preprocess_nr(dataset):
-        print(f'Pre-processing...{dataset}')
-        preprocessing(dataset)
-        run_nr(dataset)
-    preprocess_nr('PostAF2')
-    preprocess_nr('PreAF2')
-    preprocess_nr('Everything')
+    df_train = preprocessing('Everything')
+    df_test = preprocessing('new_files')
 
 if args.process:
     print('Processing...')
-    runGBReg('PreAF2', 'PostAF2')
-    runGBReg('PreAF2', 'Everything')
+    result_df = runGBReg(df_train, df_test)
 
 if args.postprocess:
     print('Postprocessing...')
-    postprocessing('PostAF2')
-    postprocessing('Everything')
+    postprocessing(result_df, 'new_files')
 
 if args.latex:
     print('Generating LaTeX...')
-    ltp.generate_latex('PostAF2', 'Everything', 'GBReg_PostAF2')
+    # ltp.generate_latex('PostAF2', 'Everything', 'GBReg_PostAF2')
 
 print('Goodbye!')
