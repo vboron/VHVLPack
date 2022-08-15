8b6d4cdf2d00259e7018b4426470a2d7113114a5
vboron <ucbtvbo@ucl.ac.uk>
2022-08-15 14:33:25 +0100

    Run all functions

--- gbr_angle_prediction.py
+++ gbr_angle_prediction.py
@@ -42,33 +42,39 @@ def postprocessing(dataset, df):
 
 
 # *************************************************************************
-parser = argparse.ArgumentParser(description='Program for compiling angles')
-parser.add_argument('--preprocess', action='store_true', default=False)
-parser.add_argument('--process', action='store_true', default=False)
-parser.add_argument('--postprocess', action='store_true', default=False)
-parser.add_argument('--latex', action='store_true', default=False)
+# parser = argparse.ArgumentParser(description='Program for compiling angles')
+# parser.add_argument('--preprocess', action='store_true', default=False)
+# parser.add_argument('--process', action='store_true', default=False)
+# parser.add_argument('--postprocess', action='store_true', default=False)
+# parser.add_argument('--latex', action='store_true', default=False)
 
-args = parser.parse_args()
+# args = parser.parse_args()
 
-if not args.preprocess and not args.process and not args.postprocess and not args.latex:
-    print('Neither --preprocess nor --process nor --postprocess nor --latex has been specified. Enabling all of them.')
-    args.process = args.postprocess = args.latex = True
+# if not args.preprocess and not args.process and not args.postprocess and not args.latex:
+#     print('Neither --preprocess nor --process nor --postprocess nor --latex has been specified. Enabling all of them.')
+#     args.process = args.postprocess = args.latex = True
 
 
-if args.preprocess:
-    df_train = preprocessing('Everything')
-    df_test = preprocessing('new_files')
+# if args.preprocess:
+#     df_train = preprocessing('Everything')
+#     df_test = preprocessing('new_files')
 
-if args.process:
-    print('Processing...')
-    result_df = runGBReg(df_train, df_test, 'train_Everything_test_abdbnew')
+# if args.process:
+#     print('Processing...')
+#     result_df = runGBReg(df_train, df_test, 'train_Everything_test_abdbnew')
 
-if args.postprocess:
-    print('Postprocessing...')
-    postprocessing(result_df, 'new_files')
+# if args.postprocess:
+#     print('Postprocessing...')
+#     postprocessing(result_df, 'new_files')
 
-if args.latex:
-    print('Generating LaTeX...')
-    # ltp.generate_latex('PostAF2', 'Everything', 'GBReg_PostAF2')
+# if args.latex:
+#     print('Generating LaTeX...')
+#     # ltp.generate_latex('PostAF2', 'Everything', 'GBReg_PostAF2')
 
+df_train = preprocessing('Everything')
+df_test = preprocessing('new_files')
+print('Processing...')
+result_df = runGBReg(df_train, df_test, 'train_Everything_test_abdbnew')
+print('Postprocessing...')
+postprocessing(result_df, 'new_files')
 print('Goodbye!')
