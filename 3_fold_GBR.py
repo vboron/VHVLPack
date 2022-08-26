d3724f64990161d7a607c38747c6ef1dba9bee70
vboron <ucbtvbo@ucl.ac.uk>
2022-08-26 14:15:47 +0100

    Remove unneeded reset_index()

--- 3_fold_GBR.py
+++ 3_fold_GBR.py
@@ -23,7 +23,6 @@ def preprocessing(ds, set_name):
 def combine_dfs(list_of_dfs):
     # dfs = [df.set_index('code') for df in list_of_dfs]
     df = pd.concat(list_of_dfs, axis=1)
-    df = df.reset_index()
     return df
 
 
