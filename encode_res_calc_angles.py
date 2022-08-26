55ec014e946e4135f58845b3d98a0af340ebfd15
vboron <ucbtvbo@ucl.ac.uk>
2022-08-26 11:46:15 +0100

    Remove print

--- encode_res_calc_angles.py
+++ encode_res_calc_angles.py
@@ -77,7 +77,6 @@ def read_pdbfiles_as_lines(directory) -> pd.DataFrame:
                         atom_lines.append(data)
 
     df = pd.DataFrame(data=atom_lines, columns=col)
-    print(df)
     return df
 
 
