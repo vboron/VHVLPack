dfcc3a1f597e10ac0c152d1944364d48fdf29c81
vboron <ucbtvbo@ucl.ac.uk>
2022-08-30 17:46:06 +0100

    Add print

--- sklearn_methods.py
+++ sklearn_methods.py
@@ -108,8 +108,8 @@ def run_GradientBoostingClassifier(X_test, df: pd.DataFrame, model_name):
     y_pred = pickle_model.predict(X_test)
     print(y_pred)
     df['predicted'] = y_pred
+    print(df['predicted'])
     df['result'] = (df['class'].apply(set) - df['predicted'].apply(set)).astype(bool)
-    print(df)
     return df
 
 
