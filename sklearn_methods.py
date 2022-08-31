eeb8296bb8cbe59b3904829078798f844ffaa7c3
vboron <ucbtvbo@ucl.ac.uk>
2022-08-31 13:06:50 +0100

    Add print of values in end table

--- sklearn_methods.py
+++ sklearn_methods.py
@@ -118,6 +118,7 @@ def run_GradientBoostingClassifier(X_test, df: pd.DataFrame, model_name):
     y_pred = pickle_model.predict(X_test)
     df['predicted'] = y_pred
     df['result'] = (df['class'].apply(set) - df['predicted'].apply(set)).astype(bool)
+    print(df['result'].value_counts())
     return df
 
 
