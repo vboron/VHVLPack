32dc07c2e6e4768ae8e3bb8d97da533065b031a1
vboron <ucbtvbo@ucl.ac.uk>
2022-09-05 12:35:42 +0100

    remove first letter of file

--- change_files_to_uppercase.py
+++ change_files_to_uppercase.py
@@ -4,10 +4,10 @@ import sys
 
 cwd = os.getcwd()
 for file in os.listdir(sys.argv[1]):
-    if file.endswith('.mar') or file.endswith('.cho'):
-        src = os.path.join(cwd, sys.argv[1], file)
-        dst = os.path.join(cwd, sys.argv[1], (file[2:-4].upper() + '.pdb'))
-    # if file.endswith('.pdb'):
+    # if file.endswith('.mar') or file.endswith('.cho'):
     #     src = os.path.join(cwd, sys.argv[1], file)
-    #     dst = os.path.join(cwd, sys.argv[1], (file[1:7] + '.pdb'))
+    #     dst = os.path.join(cwd, sys.argv[1], (file[2:-4].upper() + '.pdb'))
+    if file.endswith('.pdb'):
+        src = os.path.join(cwd, sys.argv[1], file)
+        dst = os.path.join(cwd, sys.argv[1], (file[1:]))
         os.rename(src, dst)
