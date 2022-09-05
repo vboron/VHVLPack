d5be131942d58a6d78404bc39fd3c424331da724
vboron <ucbtvbo@ucl.ac.uk>
2022-09-05 12:03:37 +0100

    Added help to args, make name of new dir an arg

--- compare_dir_files.py
+++ compare_dir_files.py
@@ -4,14 +4,14 @@ import argparse
 import os
 import shutil
 
-def compare_dirs(dir1, dir2):
+def compare_dirs(dir1, dir2, new_dir_name):
     dir1_files = os.listdir(dir1)
     dir2_files = os.listdir(dir2)
     new_files = []
     for file in dir2_files:
         if file not in dir1_files:
             new_files.append(file)
-    new_dir = 'new_files'
+    new_dir = new_dir_name
     print(len(new_files))
     i=0
     for file in new_files:
@@ -24,9 +24,10 @@ def compare_dirs(dir1, dir2):
 
 
 parser = argparse.ArgumentParser(description='Program for compiling angles')
-parser.add_argument('--dir1', required=True)
-parser.add_argument('--dir2', required=True)
+parser.add_argument('--dir1', required=True, help='Directory which is used as base')
+parser.add_argument('--dir2', required=True, help='Directory from which files will be copied into new directory')
+parser.add_argument('--new_dir_name', required=True, help='Name for new directory which will be created')
 
 args = parser.parse_args()
 
-compare_dirs(args.dir1, args.dir2)
+compare_dirs(args.dir1, args.dir2, args.new_dir_name)
