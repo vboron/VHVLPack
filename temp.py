f04e79607c3edd0adc9c97d95e321c4b5772317f
vboron <ucbtvbo@ucl.ac.uk>
2022-09-26 22:23:51 +0100

    Change src for files

--- temp.py
+++ temp.py
@@ -12,15 +12,13 @@ def compare_dirs(dir1, dir2):
     while i <= len(dir1_files)//10:
         new_files.append(dir1_files[i])
         i+=1
-    new_dir = dir2
-    os.mkdir(new_dir)
+    os.mkdir(dir2)
     print(len(new_files))
 
     for file in new_files:
-
         print(file)
-        src = os.path.join(dir2, file)
-        dst = os.path.join(new_dir, file)
+        src = os.path.join(dir1, file)
+        dst = os.path.join(dir2, file)
         shutil.copy2(src, dst)
 
 
