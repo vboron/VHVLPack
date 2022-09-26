5adc09da37356a24955dfae10008552312fbfac4
vboron <ucbtvbo@ucl.ac.uk>
2022-09-26 22:18:26 +0100

    Init make 10% set

---
+++ temp.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import shutil
+
+def compare_dirs(dir1, dir2):
+    dir1_files = os.listdir(dir1)
+
+    new_files = []
+    i=0
+    while i <= len(dir1_files)//10:
+        new_files.append(dir1_files[i])
+        i+=1
+    new_dir = dir2
+    os.mkdir(new_dir)
+    print(len(new_files))
+
+    for file in new_files:
+
+        print(file)
+        src = os.path.join(dir2, file)
+        dst = os.path.join(new_dir, file)
+        shutil.copy2(src, dst)
+
+
+parser = argparse.ArgumentParser(description='Program for compiling angles')
+parser.add_argument('--dir1', required=True, help='Directory which is used as base')
+parser.add_argument('--dir2', required=True, help='Directory from which files will be copied into new directory')
+
+args = parser.parse_args()
+
+compare_dirs(args.dir1, args.dir2)
