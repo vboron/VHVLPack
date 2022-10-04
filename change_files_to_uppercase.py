6c46dd388cbbf9de219d31615bbed90da5d73bdd
vboron <ucbtvbo@ucl.ac.uk>
2022-10-04 21:00:48 +0100

    save

--- change_files_to_uppercase.py
+++ change_files_to_uppercase.py
@@ -5,12 +5,12 @@ import shutil
 
 cwd = os.getcwd()
 for file in os.listdir(sys.argv[1]):
-    # if file.endswith('.mar'):
-    #     src = os.path.join(cwd, sys.argv[1], file)
-    #     dst = os.path.join(cwd, sys.argv[1], (file[2:-4].upper() + '.pdb'))
-    #     # os.rename(src, dst)
-        # shutil.copy2(src, dst)
-    if file.endswith('.pdb'):
+    if file.endswith('.mar'):
         src = os.path.join(cwd, sys.argv[1], file)
-        dst = os.path.join(cwd, sys.argv[1], (file[1:6] + '.pdb'))
-        os.rename(src, dst)
+        dst = os.path.join(cwd, sys.argv[1], (file[3:6].upper() + '.pdb'))
+        # os.rename(src, dst)
+        shutil.copy2(src, dst)
+    # if file.endswith('.pdb'):
+    #     src = os.path.join(cwd, sys.argv[1], file)
+    #     dst = os.path.join(cwd, sys.argv[1], (file[1:6] + '.pdb'))
+    #     os.rename(src, dst)
