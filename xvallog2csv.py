10888cceab90d1b806d4efa344ae7624f81aae15
vboron <ucbtvbo@ucl.ac.uk>
2022-10-11 14:25:51 +0100

    Try

--- xvallog2csv.py
+++ xvallog2csv.py
@@ -74,7 +74,7 @@ def runGBReg(df: pd.DataFrame, model_name: str, graph_name: str, graph_dir) -> p
         # df.to_csv(os.path.join(
         #     graph_dir, f'results_for_{model_name}.csv'), index=False)
     # df2 = df2.groupby('angle').mean('predicted')
-    df=df.groupby('angle')
+    df=df.groupby(by='angle')
     print(df)
     final = df2.merge(df, on='angle')
     print('final:', final)
