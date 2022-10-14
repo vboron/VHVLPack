8019e1ded160fde35c4eded340577a9bed37e44c
vboron <ucbtvbo@ucl.ac.uk>
2022-10-14 11:23:18 +0100

    combine files

--- temp.py
+++ temp.py
@@ -4,74 +4,29 @@
 # --modelname 'train_Everything_H100G_residues_considered_nosubsampling' 
 # --graphname 'train_everything_test_abdbnew_nosubsampling'
 # # *************************************************************************
-import os
 import argparse
-import shutil
-import utils
-import encode_res_calc_angles as erca
-import nonred
-import graphing
-from sklearn_methods import *
-import numpy as np
+import pandas as pd
 
 
 # *************************************************************************
-def preprocessing(ds):
-    print('Extracting angles and residues, and encoding...')
-    encoded_df, ang_df = erca.extract_and_export_packing_residues(
-        ds, ds, '4d.dat')
-    print('Nonredundantizing...')
-    nonred_df = nonred.NR2(encoded_df, ds, f'{ds}_NR2_13res')
-    return nonred_df, ang_df
+def combine(in_with_code, in_with_pred, out):
+    code_df = pd.read_csv(in_with_code)
+    code_list = code_df['code'].to_list()
 
-
-# *************************************************************************
-def runGBReg(train_df: pd.DataFrame, test_df: pd.DataFrame, model_name: str, graph_name: str, graph_dir) -> pd.DataFrame:
-    if '/' in graph_dir:
-        graph_dir = graph_dir.replace('/', '')
-    print('Making train and test sets...')
-    X_train, y_train, _x_, X_test, y_true, df_test = make_reg_sets_from_df(train_df, test_df)
-    print('Building ML model...')
-    gbr = build_GradientBoostingRegressor_model( X_train, y_train,model_name)
-    print('Running ML...')
-    df = run_GradientBoostingRegressor(X_test, df_test, model_name)
-    df.to_csv(os.path.join(graph_dir, f'results_for_{model_name}.csv'), index=False)
-    # print('Plotting deviance...')
-    # plot_deviance(gbr, os.path.join(graph_dir, f'{graph_name}_deviance'), X_test, y_true)
-    return df
-
-
-# *************************************************************************
-def postprocessing(df, dataset, ang_df, name):
-    graphing.actual_vs_predicted_from_df(df, dataset, name, f'{name}_pa')
-    graphing.sq_error_vs_actual_angle(
-        dataset, df, f'{name}_sqerror_vs_actual')
-    graphing.angle_distribution(
-        dataset, ang_df, f'{name}_angledistribution')
-    graphing.error_distribution(
-        dataset, df, f'{name}_errordistribution')
+    pred_df = pd.read_csv(in_with_pred)
+    pred_df = pred_df[['code', 'predicted']]
+    df = pred_df.loc[pred_df['code'].isin(code_list)]
+    
+    print(df)
 
 
 # *************************************************************************
 parser = argparse.ArgumentParser(description='Program for compiling angles')
-parser.add_argument('--trainset', required=True, help='directory of pdb files used for training model', type=str)
-parser.add_argument('--testset', required=True, help='directory of pdb files used for testing model', type=str)
-parser.add_argument('--modelname', required=True, help='name which will be given to the model that is trained', type=str)
-parser.add_argument('--graphname', required=True, help='name which will be included in the graphs', type=str)
-# parser.add_argument('--latex', action='store_true', default=False)
+parser.add_argument('--in1', required=True, help='input csv', type=str)
+parser.add_argument('--in2', required=True, help='input csv', type=str)
+parser.add_argument('--out', required=True, help='output csv name', type=str)
 
 args = parser.parse_args()
 
-# if args.latex:
-#     print('Generating LaTeX...')
-#     # ltp.generate_latex('PostAF2', 'Everything', 'GBReg_PostAF2')
-print(f'Preprocessing {args.trainset}...')
-df_train, train_angles = preprocessing(args.trainset)
-print(f'Preprocessing {args.testset}...')
-df_test, test_angles= preprocessing(args.testset)
-print('Processing...')
-result_df = runGBReg(df_train, df_test, args.modelname, args.graphname, args.testset)
-print(result_df)
-print('Postprocessing...')
-postprocessing(result_df, args.testset, test_angles, args.graphname)
-print('Goodbye!')
+
+print('Bye bye bye!')
