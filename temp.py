56280ae6c71aa1fdc1a3141cf6deb2e3a9d8db01
vboron <ucbtvbo@ucl.ac.uk>
2022-10-20 16:38:32 +0100

    Export file

--- temp.py
+++ temp.py
@@ -4,14 +4,10 @@ import argparse
 
 def combine_dfs(code_file, pred_file):
     code_df = pd.read_csv(code_file)
-    print(code_df)
     pred_df = pd.read_csv(pred_file)
-    print(pred_df)
     final = code_df.merge(pred_df, on='code')
-    print(final)
     final = final[['code', 'predicted']]
-    print(final)
-    # final.to_csv('')
+    final.to_csv('repacking_angles.csv', index=False)
 
 parser = argparse.ArgumentParser(description='Program for compiling angles')
 parser.add_argument('--code', required=True, type=str)
