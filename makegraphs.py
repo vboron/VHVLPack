071b3f6f7d5e01c214ffe66eb47e1280a70764ea
vboron <ucbtvbo@ucl.ac.uk>
2022-10-26 16:59:21 +0100

    save

--- makegraphs.py
+++ makegraphs.py
@@ -4,16 +4,18 @@ from graphing import *
 import pandas as pd
 import argparse
 
-def makegraphs(csv, output, dire):
+def makegraphs(csv, output, dire, df_stat):
     df = pd.read_csv(csv)
     error_distribution(dire, df, output)
     sq_error_vs_actual_angle(dire, df, output)
+    actual_vs_predicted_from_df(df, dire, df_stat, output)
 
 
 parser = argparse.ArgumentParser(description='Program for extracting VH/VL relevant residues')
 parser.add_argument('--dir', required=True)
 parser.add_argument('--out', required=True)
 parser.add_argument('--csv', required=True)
+parser.add_argument('--stats', required=True)
 args = parser.parse_args()
 
-makegraphs(args.csv, args.out, args.dir)
+makegraphs(args.csv, args.out, args.dir, args.stat)
