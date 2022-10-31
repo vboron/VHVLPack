dc450f71468333342eceeae2b00d4924a7935584
vboron <ucbtvbo@ucl.ac.uk>
2022-10-31 16:30:17 +0000

    Fix arguments

--- plotrepackrmsd.py
+++ plotrepackrmsd.py
@@ -5,27 +5,30 @@ import pandas as pd
 import argparse
 import os
 
-def plotgraphs(dataset, column):
+def readandplot(file):
+    df_ = pd.read_csv(file)
+    plotgraphs(df_, 'abYmod', 'RMSD abYmod')
+    plotgraphs(df_, 'repacked', 'RMSD Repacked abYmod')
 
-    plt.figure()
+    def plotgraphs(df, dataset, column):
+        plt.figure()
 
-    df = df.round({column: 1})
+        df = df.round({column: 1})
 
-    df_count=df[column].value_counts().sort_index()
-    plt.plot(df_count)
-    axes = plt.gca()
-    axes.set_xlim([0, 6])
-    axes.set_ylim([0, 250])
-    plt.xlabel('RMSD')
-    plt.ylabel('Frequency')
+        df_count=df[column].value_counts().sort_index()
+        plt.plot(df_count)
+        axes = plt.gca()
+        axes.set_xlim([0, 6])
+        axes.set_ylim([0, 250])
+        plt.xlabel('RMSD')
+        plt.ylabel('Frequency')
 
-    plt.tight_layout()
-    plt.savefig(f'{dataset}.jpg', format='jpg')
-    plt.close()
+        plt.tight_layout()
+        plt.savefig(f'{dataset}.jpg', format='jpg')
+        plt.close()
 
 parser = argparse.ArgumentParser(description='Program for extracting VH/VL relevant residues')
 parser.add_argument('--csv', required=True)
 args = parser.parse_args()
 
-plotgraphs('abYmod', 'RMSD abYmod')
-plotgraphs('repacked', 'RMSD Repacked abYmod')
+readandplot(args.csv)
