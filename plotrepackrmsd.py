d7c34d8499e8eb53b738bf3c0bfd07e57b70af9c
vboron <ucbtvbo@ucl.ac.uk>
2022-10-31 16:36:21 +0000

    Adjust axes

--- plotrepackrmsd.py
+++ plotrepackrmsd.py
@@ -16,8 +16,8 @@ def readandplot(file):
         df_count=df[column].value_counts().sort_index()
         plt.plot(df_count)
         axes = plt.gca()
-        axes.set_xlim([0, 6])
-        axes.set_ylim([0, 150])
+        axes.set_xlim([0, 4])
+        axes.set_ylim([0, 120])
         plt.xlabel('RMSD')
         plt.ylabel('Frequency')
 
