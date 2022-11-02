da765a06c55752a60316c1e2514557ea2c34be61
vboron <ucbtvbo@ucl.ac.uk>
2022-11-02 12:18:36 +0000

    pivot without index

--- runGBRegmodel.py
+++ runGBRegmodel.py
@@ -113,7 +113,6 @@ def prep_table(df):
 def encode_df(df):
     df = df.drop_duplicates(subset=['code', 'L/H position'], keep='first')
     df = df.drop(columns='code')
-    print(df)
     good_positions = ['L32', 'L34', 'L36', 'L38', 'L40', 'L41', 'L43', 'L44', 'L46', 'L50', 'L86', 'L87', 'L89', 'L91', 
                       'L96', 'L98', 'H33', 'H35', 'H39', 'H42', 'H45', 'H47', 'H50', 'H60', 'H62', 'H91', 'H99', 'H100', 
                       'H100A', 'H100B', 'H100C', 'H100D', 'H100E', 'H100F', 'H1003G', 'H103', 'H105']
@@ -121,11 +120,9 @@ def encode_df(df):
     col_list = df['L/H position'].values.tolist()
     for pos in good_positions:
         if pos not in col_list:
-            print(pos)
             new_row = pd.Series({'L/H position': pos, 'residue': float('nan')})
             df = pd.concat([df, new_row.to_frame().T], ignore_index=True)
-    print(df)
-    df_piv = df.pivot_table(index='code', columns='L/H position', values='residue', aggfunc='sum')
+    df_piv = df.pivot_table(columns='L/H position', values='residue', aggfunc='sum')
     df = df_piv.reset_index()
     df = df.rename_axis(None, axis=1)
     
