a79adac100d0f75ff5e3085904bee15b199c8e92
vboron <ucbtvbo@ucl.ac.uk>
2022-11-07 12:27:05 +0000

    graph angle distribution

--- temp.py
+++ temp.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 import pandas as pd
 import argparse
+from graphing import angle_distribution as angd
 
-def combine_dfs(code_file, pred_file):
-    code_df = pd.read_csv(code_file)
-    pred_df = pd.read_csv(pred_file)
-    final = code_df.merge(pred_df, on='code')
-    final = final[['code', 'predicted']]
-    final.to_csv('repacking_angles.csv', index=False)
+def runf(csv, dir):
+    df_all = pd.read_csv(csv)
+    df = df_all[['code', 'angle']]
+    angd(dir, df, 'sept_ang_dist')
 
 parser = argparse.ArgumentParser(description='Program for compiling angles')
-parser.add_argument('--code', required=True, type=str)
-parser.add_argument('--pred', required=True, type=str)
+parser.add_argument('--csv', required=True, type=str)
+parser.add_argument('--dir', required=True, type=str)
 
 args = parser.parse_args()
-combine_dfs(args.code, args.pred)
+runf(args.csv, args.dir)
